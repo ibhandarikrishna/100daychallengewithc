584a462f612b3bd4f74666c4d33393d89e8c2638
Krishna Bhandari <115021507+ibhandarikrishna@users.noreply.github.com>
2023-03-14 22:19:23 +0530

    Create Sum Root to Leaf Numbers.cpp

---
+++ day_157/Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+    
+    int dfs(TreeNode* root, int sum) {
+        if (!root) {
+            return 0;
+        }
+        sum = sum * 10 + root->val;
+        if (!root->left && !root->right) {
+            return sum;
+        }
+        return dfs(root->left, sum) + dfs(root->right, sum);
+    }
+};
